bfc03692cd7687970610590e331f9d3727385c52
takatsu <takatsu@chinou.co.jp>
2021-02-04 18:40:49 +0900

    Add: touch, untouch, inrange

--- BLE_HID_test.ino
+++ BLE_HID_test.ino
@@ -77,15 +77,30 @@ void setup() {
     0x85, 0x04,                         //   REPORT_ID (4)
     0x09, 0x22,                         //   USAGE (Finger)
     0xa1, 0x00,                         //   COLLECTION (Physical)
+    // tipawitch & untouch
     0x09, 0x42,                         //     USAGE (Tip Switch)
-    0x09, 0x51,                         //     USAGE (Contact Identifier) *added line
+    0x09, 0x32,                         //     USAGE (InRange)
+    0x09, 0x33,                         //     USAGE (Touch)
+    0x09, 0x34,                         //     USAGE (Untouch)
     0x15, 0x00,                         //     LOGICAL_MINIMUM (0)
     0x25, 0x01,                         //     LOGICAL_MAXIMUM (1)
     0x75, 0x01,                         //     REPORT_SIZE (1)
-    0x95, 0x02,                         //     REPORT_COUNT (2)
+    0x95, 0x04,                         //     REPORT_COUNT (4)
     0x81, 0x02,                         //     INPUT (Data,Var,Abs)
-    0x95, 0x0e,                         //     REPORT_COUNT (14)
+    // dummy for untouch
+    0x95, 0x04,                         //     REPORT_COUNT (4)
     0x81, 0x03,                         //     INPUT (Cnst,Var,Abs)
+    // contactID
+    0x09, 0x51,                         //     USAGE (Contact Identifier) *added line
+    0x15, 0x00,                         //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                         //     LOGICAL_MAXIMUM (9)
+    0x75, 0x01,                         //     REPORT_SIZE (1)
+    0x95, 0x01,                         //     REPORT_COUNT (1)
+    0x81, 0x02,                         //     INPUT (Data,Var,Abs)
+    // dummy for ContactID
+    0x95, 0x07,                         //     REPORT_COUNT (7)
+    0x81, 0x03,                         //     INPUT (Cnst,Var,Abs)
+    // X and Y on Desktop
     0x05, 0x01,                         //     USAGE_PAGE (Generic Desktop)
     0x75, 0x10,                         //     REPORT_SIZE (16)
     0x95, 0x01,                         //     REPORT_COUNT (1)
@@ -128,31 +143,32 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int untouched = 0;
   if (connected) {
     if (Serial.available() > 0) {
       String func;
       int x;
       int y;
-      int s;
-      int c;
+      int u; //untouch
+      int c; //contactID
       func = Serial.readStringUntil(';');
       x = func.substring(0, 4).toInt();
       y = func.substring(4, 8).toInt();
-      s = func.substring(8, 9).toInt();
+      u = func.substring(8, 9).toInt();
       c = func.substring(9).toInt();
       Serial.println(x);
       Serial.println(y);
-      Serial.println(s);
+      Serial.println(u);
       Serial.println(c);
-      if (s == 2 and c == 2) {
+      if (u == 2 and c == 2) {
         zoomin(x, y);
-      } else if (s == 3 and c == 3) {
+      } else if (u == 3 and c == 3) {
         zoomout(x, y);
       } else if (screenX0 < x and x <= screenX1) {
         if (screenY0 < y and y <= screenY1) {
-          if (c == 0 or c == 1) {
-            if (s == 0 or s == 1) {
-              send_multi(x, y, s, c);
+          if (c >= 0 or c <= 9) {
+            if (u == 0 or u == 1) {
+              send_multi(x, y, u, c);
             }
           }
         } else {
@@ -161,66 +177,47 @@ void loop() {
       } else {
         Serial.println("x-value: out of range");
       }
-
+      untouched = 0;
     } else {
       delay(500);
-      send_null();
-      Serial.println("Clearing Touch");
+      if (untouched == 0) {
+        send_null();
+        Serial.println("Clearing Touch");
       }
-  } else{
+      untouched = 1;
+    }
+  } else {
     Serial.println("Waiting for Connection");
     delay(500);
-    }
+  }
 }
 
 void send_null() {
   uint8_t m[6];
-  m[0] = 0;
-  m[1] = 0;
-  m[2] = 0;
-  m[3] = 0;
-  m[4] = 0;
-  m[5] = 0;
-  input->setValue(m, 6);
-  input->notify();
-  }
-
-void send(int paramX, int paramY, int _switch) {
-  logicalX = map(paramX, screenX0, screenX1, logicalMinX, logicalMaxX);
-  logicalY = map(paramY, screenY0, screenY1, logicalMinY, logicalMaxY);
-  uint8_t m[6];
-  m[0] = _switch | _switch;//  m[0] = 0x10 | _switch;
-  m[1] = 0;
-  m[2] = LSB(logicalX);
-  m[3] = MSB(logicalX);
-  m[4] = LSB(logicalY);
-  m[5] = MSB(logicalY);
+  m[0] = 0; // untouched
+  m[1] = 0; // contactID
+  m[2] = 0; // LSB(X)
+  m[3] = 0; // MSB(X)
+  m[4] = 0; // LSB(Y)
+  m[5] = 0; // MSB(Y)
   input->setValue(m, 6);
   input->notify();
 }
 
-void send_multi(int paramX, int paramY, int _switch, int _cid) {
+void send_multi(int paramX, int paramY, int _untouch, int _cid) {
   logicalX = map(paramX, screenX0, screenX1, logicalMinX, logicalMaxX);
   logicalY = map(paramY, screenY0, screenY1, logicalMinY, logicalMaxY);
   uint8_t m[6];
-  if (_switch == 1) {
-    if (_cid == 0) {
-      m[0] = 0x01;
-    } else if (_cid == 1) {
-      m[0] = 0x03;
-    }
-  } else if (_switch == 0) {
-    if (_cid == 0) {
-      m[0] = 0x00;
-    } else if (_cid == 1) {
-      m[0] = 0x02;
-    }
+  if (_untouch == 0){
+    m[0] = 0x05; // s=1,i=0,t=1,u=0 => 1010 invert 0101
+  }else{
+    m[0] = 0x08; // s=0,i=0,t=0,u=1 => 0001 invert 1000
   }
-  m[1] = 0;
-  m[2] = LSB(logicalX);
-  m[3] = MSB(logicalX);
-  m[4] = LSB(logicalY);
-  m[5] = MSB(logicalY);
+  m[1] = _cid; // contactID
+  m[2] = LSB(logicalX); // LSB(X)
+  m[3] = MSB(logicalX); // MSB(X)
+  m[4] = LSB(logicalY); // LSB(Y)
+  m[5] = MSB(logicalY); // MSB(Y)
   input->setValue(m, 6);
   input->notify();
 }
@@ -230,19 +227,19 @@ void zoomin(int paramX, int paramY) {
       int x0, x1;
       for (int i = 0; i < 100; i++) {
 
-        x0 = paramX - i*2 -100;
-        x1 = paramX + i*2 +100;
+        x0 = paramX - i * 2 - 100;
+        x1 = paramX + i * 2 + 100;
         if (x0 < screenX0) {
           x0 = screenX0;
         }
         if (x1 > screenX1) {
           x1 = screenX1;
         }
-        send_multi(x0, paramY, 1, 0);
-        send_multi(x1, paramY, 1, 1);
+        send_multi(x0, paramY, 0, 0);
+        send_multi(x1, paramY, 0, 1);
       }
-      send_multi(x0, paramY, 0, 0);
-      send_multi(x1, paramY, 0, 1);
+      send_multi(x0, paramY, 1, 0);
+      send_multi(x1, paramY, 1, 1);
     } else {
       Serial.println("y-value: out of range");
     }
@@ -256,20 +253,20 @@ void zoomout(int paramX, int paramY) {
       int x0, x1;
       for (int i = 0; i < 100; i++) {
 
-        x0 = paramX + i*2 - 200;
-        x1 = paramX - i*2 + 200;
+        x0 = paramX + i * 2 - 200;
+        x1 = paramX - i * 2 + 200;
         if (x0 < screenX0) {
           x0 = screenX0;
         }
         if (x1 > screenX1) {
           x1 = screenX1;
         }
-        send_multi(x0, paramY, 1, 0);
-        send_multi(x1, paramY, 1, 1);
+        send_multi(x0, paramY, 0, 0);
+        send_multi(x1, paramY, 0, 1);
       }
       delay(5);
-      send_multi(x0, paramY, 0, 0);
-      send_multi(x1, paramY, 0, 1);
+      send_multi(x0, paramY, 1, 0);
+      send_multi(x1, paramY, 1, 1);
     } else {
       Serial.println("y-value: out of range");
     }
